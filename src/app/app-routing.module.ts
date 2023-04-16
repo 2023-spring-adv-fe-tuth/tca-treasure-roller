5d08ce1e8dffea303cbea88665b45d8fc1c5e076
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-15 20:16:00 -0500

    Update app-routing.module.ts
    copy paste from old app
    hope I didn't bring some crap from that messy backend disaster

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
+import { ParametersScreenComponent } from './parameters-screen/parameters-screen.component';
+import { ResultScreenComponent } from './result-screen/result-screen.component';
+import { HoardScreenComponent } from './hoard-screen/hoard-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeScreenComponent},
+  {path: 'home-screen', component: HomeScreenComponent},
+  {path: 'parameters-screen', component: ParametersScreenComponent},
+  {path: 'result-screen', component: ResultScreenComponent},
+  {path: 'hoard-screen', component: HoardScreenComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    routes,
+    {useHash: true}
+    )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
