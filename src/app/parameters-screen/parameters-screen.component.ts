0b7f4e2f0ab603e09643cb486b71281cec9ff4f7
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-15 20:25:17 -0500

    Update parameters-screen.component.ts
    opposite order...

--- src/app/parameters-screen/parameters-screen.component.ts
+++ src/app/parameters-screen/parameters-screen.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { TreasureSvcService } from './../treasure-svc.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-parameters-screen',
   templateUrl: './parameters-screen.component.html',
   styleUrls: ['./parameters-screen.component.css']
 })
-export class ParametersScreenComponent {
+export class ParametersScreenComponent implements OnInit {
+
+  constructor(
+    private router: Router,
+    private svc: TreasureSvcService
+  ) {};
+
+  extraPlayers: {
+    name: string;
+    checked: boolean;
+  }[] = [];
+
+  ngOnInit(): void {
+    this.extraPlayers = this.svc.getPlayerNames().map(x => ({
+      name: x,
+      checked: false
+    }));
+
+    console.log(this.extraPlayers);
+  };
+
+  rollTreasure = () => {
+    this.router.navigateByUrl("result-screen");
+  };
 
 }
