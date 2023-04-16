17b70fada22e4aa41c8206e7091c49314d84d9fb
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-15 20:26:16 -0500

    Update result-screen.component.ts
    more paste

--- src/app/result-screen/result-screen.component.ts
+++ src/app/result-screen/result-screen.component.ts
@@ -1,10 +1,34 @@
+import { TreasureSvcService } from './../treasure-svc.service';
+import { Encounter, Loot } from './../treasure-types';
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-result-screen',
   templateUrl: './result-screen.component.html',
   styleUrls: ['./result-screen.component.css']
 })
-export class ResultScreenComponent {
 
+export class ResultScreenComponent {
+  constructor(
+    private location: Location,
+    private svc: TreasureSvcService
+  ) {};
+
+  addMe = this.svc.otherHoard;
+  showMe = this.svc.itsTwo;
+  reRoll = this.svc.reroll;
+
+  saveTreasure = () => {
+    this.svc.addGameResult({
+      winner: "Foxswallow",
+      players: ["Foxswallow", "Minion One", "Minion Two"]
+    });
+    this.svc.forgeHistory(this.addMe);
+    this.location.historyGo(-2);
+  };
+
+  rerollTreasure = () => {
+    this.showMe = this.reRoll;
+  };
 }
