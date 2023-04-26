b4bbf5b96ff1e63a5d61426f791f8c9d735c8c08
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-25 20:57:23 -0500

    it worked again!!
    I am a genius, right? Yeah. Saving it to the home should be a breeze, right? should I cross any fingers/toes?

--- src/app/result-screen/result-screen.component.ts
+++ src/app/result-screen/result-screen.component.ts
@@ -16,11 +16,27 @@ export class ResultScreenComponent implements OnInit {
   ) {};
 
   openChest: Loot[] = [];
-  encounterLoot: any = [];
+  encounterLoot = gatherLoot(this.svc.charts, this.openChest, this.svc.hoardSetup.budget);
   addMe = this.svc.otherHoard;
-  showMe: Loot[] = [];
-  reRoll = this.svc.reroll;
+  showMe: Loot[] = this.encounterLoot[0];
+  reRoll = gatherLoot(this.svc.charts, this.openChest, this.svc.hoardSetup.budget);
   
+
+  
+
+  rerollTreasure = () => {
+    this.showMe = this.reRoll[0];
+  };
+
+  ngOnInit(): void {
+    console.log(this.svc.hoardSetup);
+    if (this.svc.hoardSetup.name.length > 0) {
+      console.log("encounterLoot");
+      console.log(this.encounterLoot[0]);
+      console.log("rerollTreasure");
+      console.log(this.reRoll[0]);
+    };
+  };
 
   saveTreasure = () => {
     this.svc.addGameResult({
@@ -30,18 +46,4 @@ export class ResultScreenComponent implements OnInit {
     this.svc.forgeHistory(this.addMe);
     this.location.historyGo(-2);
   };
-
-  rerollTreasure = () => {
-    this.showMe = this.reRoll;
-  };
-
-  ngOnInit(): void {
-    console.log(this.svc.hoardSetup);
-    if (this.svc.hoardSetup.name.length > 0) {
-      this.encounterLoot = gatherLoot(this.svc.charts, this.encounterLoot, this.svc.hoardSetup.budget);
-      console.log(this.encounterLoot[0]);
-      console.log(this.encounterLoot[1]);
-      this.showMe = this.encounterLoot[0];
-    };
-  }
 }
