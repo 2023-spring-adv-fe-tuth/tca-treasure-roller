a490efe971d2cf77fc3d63a28632711058d0e5f7
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-05-01 11:49:15 -0500

    localforage input
    set input box and button for localforage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import * as localforage from 'localforage';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +9,24 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class AppComponent {
 
-constructor(private modalService: NgbModal){}
+  constructor(private modalService: NgbModal){}
 
-public open({ modal }: { modal: any; }): void {
-  this.modalService.open(modal);
-}
+  public open({ modal }: { modal: any; }): void {
+    this.modalService.open(modal);
+  }
+
+  emailAddress = "";
+
+  saveEmail = async() => {
+    try {
+      await localforage.setItem( // setItem takes key-value pair
+        "emailAddress" // key
+        , this.emailAddress // value
+      )
+    } catch (err) {
+      console.log(err);
+    };
+  };
 
   title = 'tca-treasure-roller-2';
 }
